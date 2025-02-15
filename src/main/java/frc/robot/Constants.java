463742f1b1e7a76ae2d1c5ba5905ba5cbb62a6e5
rushShahay <rushhaank.sahay@gmail.com>
2025-02-14 21:44:21 -0600

    Drivetrain update

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -108,6 +108,8 @@ public class Constants {
 
   public static final class SwerveConstants {
 
+    public static final boolean isPractice = true;
+
     public static class DriverConstants {
       public static final double xDeadband = 0.0825;
       public static final double yDeadband = 0.0825;
@@ -134,10 +136,36 @@ public class Constants {
 
     public static final int PIGEON_ID = 0;
 
-    public static double LEFT_FRONT_OFFSET = 0.228027;
-    public static double RIGHT_FRONT_OFFSET = -0.099609;
-    public static double LEFT_BACK_OFFSET = -0.000244;
-    public static double RIGHT_BACK_OFFSET = -0.113525;
+    // Practice Constants
+    public static double PRACTICE_LEFT_FRONT_OFFSET = 0.372314;
+    public static double PRACTICE_RIGHT_FRONT_OFFSET = 0.485840;
+    public static double PRACTICE_LEFT_BACK_OFFSET = 0.005127;
+    public static double PRACTICE_RIGHT_BACK_OFFSET = -0.350586;
+
+    public static boolean PRACTICE_LEFT_FRONT_DRIVE_INVERTED = true;
+    public static boolean PRACTICE_RIGHT_FRONT_DRIVE_INVERTED = true;
+    public static boolean PRACTICE_RIGHT_BACK_DRIVE_INVERTED = true;
+    public static boolean PRACTICE_LEFT_BACK_DRIVE_INVERTED = false;
+
+    public static boolean PRACTICE_LEFT_FRONT_TURN_INVERTED = true;
+    public static boolean PRACTICE_RIGHT_FRONT_TURN_INVERTED = true;
+    public static boolean PRACTICE_RIGHT_BACK_TURN_INVERTED = true;
+    public static boolean PRACTICE_LEFT_BACK_TURN_INVERTED = true;
+
+    public static double LEFT_FRONT_OFFSET = 0.372314;
+    public static double RIGHT_FRONT_OFFSET = 0.485840;
+    public static double LEFT_BACK_OFFSET = 0.005127;
+    public static double RIGHT_BACK_OFFSET = -0.350586;
+
+    public static boolean LEFT_FRONT_DRIVE_INVERTED = true;
+    public static boolean RIGHT_FRONT_DRIVE_INVERTED = true;
+    public static boolean RIGHT_BACK_DRIVE_INVERTED = true;
+    public static boolean LEFT_BACK_DRIVE_INVERTED = false;
+
+    public static boolean LEFT_FRONT_TURN_INVERTED = true;
+    public static boolean RIGHT_FRONT_TURN_INVERTED = true;
+    public static boolean RIGHT_BACK_TURN_INVERTED = true;
+    public static boolean LEFT_BACK_TURN_INVERTED = true;
 
     public static final double WHEEL_DIAMETER = Units.inchesToMeters(4.00);
     public static final double DRIVE_MOTOR_GEAR_RATIO = 6.75;
