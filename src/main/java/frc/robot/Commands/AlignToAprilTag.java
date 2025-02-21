895216650f9ed0172c4ede39d33e08ff532911f0
miracle aigbogun <103385042+MiracleAig@users.noreply.github.com>
2025-02-20 21:06:20 -0600

    distance calc with trig

--- src/main/java/frc/robot/Commands/AlignToAprilTag.java
+++ src/main/java/frc/robot/Commands/AlignToAprilTag.java
@@ -39,6 +39,7 @@ public class AlignToAprilTag extends Command {
   private CommandXboxController c =
       new CommandXboxController(Constants.ControllerConstants.driverControllerPort);
 
+      
   public AlignToAprilTag(AprilTagStatsLimelight aprilTagStatsLimelight, Drivetrain drivetrain) {
     this.aprilTagStatsLimelight = aprilTagStatsLimelight;
     this.drivetrain = drivetrain;
